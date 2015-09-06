8aa938519856df82a5fb3e78a2822b3a42b7712a
gavin.w.cheng <gavin.w.cheng@gmail.com>
2015-09-06 12:22:31 +0100

    post challenge 4

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,14 +8,13 @@ describe DockingStation do
   describe "#release_bike" do
     it "releases bikes" do
     #  expect(subject.release_bike).to be_an_instance_of Bike
-      bike = Bike.new
+      bike = double :bike, working?: true
       subject.dock bike
       expect(subject.release_bike bike).to be_working
     end
 
     it "raises an error when asked to release a broken bike" do
-      bike = Bike.new
-      bike.report_broken
+      bike = double :bike, working?: false
       subject.dock bike
       expect { subject.release_bike bike }.to raise_error "Bike is broken"
     end
@@ -30,8 +29,8 @@ describe DockingStation do
   end
 
   it "raises an error when dock is full" do
-    subject.capacity.times { subject.dock Bike.new }
-    expect { subject.dock Bike.new }.to raise_error "Docking station is full"
+    subject.capacity.times { subject.dock :bike }
+    expect { subject.dock :bike }.to raise_error "Docking station is full"
   end
 
   describe "#capacity" do
